3ad39b020c2ab46ed0a666a052c9fe97e1adf9ad
unknown <azizyanasi@gmail.com>
2021-07-27 23:41:05 +0400

    added multiplication and division

--- mathExpression/ExpressionMain.java
+++ mathExpression/ExpressionMain.java
@@ -4,30 +4,40 @@ import java.util.List;
 
 public class ExpressionMain {
     public static void main(String[] args) {
-        String expression = "5 + 7 - 8 + 10";
-        double result;
-        ValueExpression temp;
+        String expression = "5 * 1 + 7 * 8 - 8 / 4";
+        double result = 0.0;
+        ValueExpression temp = null;
         List<OperatorExpression> a = OperatorExpression.generateList(expression.split(" "));
-        System.out.println(a);
-//        for (int i = 0; i < a.size(); i++) {
-//            if (a.get(i).operator.getOperator().equals("*")) {
-//                temp = new ValueExpression(a.get(i).calculate());
-//                if (i != a.size() - 1) {
-//                    a.get(i + 1).left = temp;
-//                }
-//                if (i != 0) {
-//                    a.get(i - 1).right = temp;
-//                }
-//
-//            }
-//        }
-        for (int i = 0; i < a.size() - 1; i++) {
-            if (!a.get(i).operator.getOperator().equals("*") || !a.get(i).operator.getOperator().equals("/")) {
-                a.get(i + 1).left = new ValueExpression(a.get(i).calculate());
-
+        String operator;
+        ValueExpression tempExpression;
+        for (int i = 0; i < a.size(); i++) {
+            operator = a.get(i).operator.getOperator();
+            if (operator.equals("*") || operator.equals("/")) {
+                if(temp == null)
+                    temp = new ValueExpression(a.get(i).calculate());
+                else
+                    temp.setValue(a.get(i).calculate());
+                if (i < a.size() - 1) {
+                    a.get(i + 1).left = temp;
+                }
+                if (i != 0){
+                    a.get(i - 1).right = temp;
+                }
+            }else{
+                temp = null;
             }
         }
-        System.out.println(a.get(a.size() - 1).calculate());
+
+        temp = null;
+        for (int i = 0; i < a.size(); i++) {
+            if (!a.get(i).operator.getOperator().equals("*") && !a.get(i).operator.getOperator().equals("/")) {
+                if (temp !=null){
+                    a.get(i).left = temp;
+                }
+                temp = new ValueExpression(a.get(i).calculate());
+            }
+        }
+        System.out.println(temp.getValue());
     }
 
 }
